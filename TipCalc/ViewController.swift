b71769986bbc1ce52d7704c42cf9a7772df4e538
sullyounossi <Suleiman.Younossi@gmail.com>
2016-09-16 16:16:18 -0700

    tipCalc CodePath

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Suleiman Younossi on 9/16/16.
+//  Copyright © 2016 Suleiman Younossi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+
+
+    @IBAction func calcTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
